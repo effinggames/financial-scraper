69ecc4257332a4f8fe549bae56e538daeefd7735
Rob Graeber <bgraeber@gmail.com>
2016-07-18 03:52:47 -0700

    Update UnemploymentScraper.js

--- scrapers/UnemploymentScraper.js
+++ scrapers/UnemploymentScraper.js
@@ -5,7 +5,7 @@ const Request = require('request-promise');
 const Knex = require('../util/DatabaseHelper').knex;
 const CSVParser = Promise.promisify(require('csv-parse'));
 
-const csvUrl = 'https://fred.stlouisfed.org/graph/fredgraph.csv?chart_type=line&recession_bars=on&log_scales=&bgcolor=%23e1e9f0&graph_bgcolor=%23ffffff&fo=Open+Sans&ts=12&tts=12&txtcolor=%23444444&show_legend=yes&show_axis_titles=yes&drp=0&cosd=1948-01-01&coed=2016-05-01&height=450&stacking=&range=&mode=fred&id=UNRATE&transformation=lin&nd=&ost=-99999&oet=99999&lsv=&lev=&mma=0&fml=a&fgst=lin&fgsnd=2009-06-01&fq=Monthly&fam=avg&vintage_date=&revision_date=&line_color=%234572a7&line_style=solid&lw=2&scale=left&mark_type=none&mw=2&width=1168';
+const csvUrl = 'https://fred.stlouisfed.org/graph/fredgraph.csv?chart_type=line&recession_bars=on&log_scales=&bgcolor=%23e1e9f0&graph_bgcolor=%23ffffff&fo=Open+Sans&ts=12&tts=12&txtcolor=%23444444&show_legend=yes&show_axis_titles=yes&drp=0&cosd=1948-01-01&coed=2099-05-01&height=450&stacking=&range=&mode=fred&id=UNRATE&transformation=lin&nd=&ost=-99999&oet=99999&lsv=&lev=&mma=0&fml=a&fgst=lin&fgsnd=2009-06-01&fq=Monthly&fam=avg&vintage_date=&revision_date=&line_color=%234572a7&line_style=solid&lw=2&scale=left&mark_type=none&mw=2&width=1168';
 
 class UnemploymentScraper {
     /**
